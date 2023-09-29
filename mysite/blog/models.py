2b2e2bef244d2c2c16bb8a3120a1576af71d3179
Mario Mori <mario_mori@hotmail.com>
2023-09-29 16:55:43 -0300

    Finishing chapter 2

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -19,8 +19,8 @@ class Post(models.Model):
     slug = models.SlugField(max_length=250,
                             unique_for_date="publish")
     author = models.ForeignKey(User,
-                               on_delete=models.CASCADE,
-                               related_name="blog_posts")
+                            on_delete=models.CASCADE,
+                            related_name="blog_posts")
     body = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
@@ -38,7 +38,23 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse('blog:post_detail',
-                       args=[self.publish.year,
-                             self.publish.month,
-                             self.publish.day,
-                             self.slug])
+                    args=[self.publish.year,
+                            self.publish.month,
+                            self.publish.day,
+                            self.slug])
+
+class Comment(models.Model):
+    post = models.ForeignKey(Post,
+        on_delete=models.CASCADE,
+        related_name='comments')
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    active = models.BooleanField(default=True)
+
+    class Meta:
+        ordering = ('created',)
+    def __str__(self) -> str:
+        return 'Comment by {} on {}'.format(self.name, self.post)
